75093754d6520bee1d96655333b96c9492ecbe48
Anthony Humay <ahumay@Anthonys-MacBook-Air.local>
2019-10-17 16:22:00 -0700

    .

--- code/utils.py
+++ code/utils.py
@@ -1,141 +1,235 @@
+import os
 import cv2
-import numpy
-import time
-from sklearn import neighbors, svm, cluster
+import numpy as np
+import timeit, time
+from sklearn import neighbors, svm, cluster, preprocessing
 from sklearn.cluster import AgglomerativeClustering
-from classifiers import KNN_classifier
+from sklearn.neighbors import KNeighborsClassifier
 
 def readImages(imagesRaw, newSize):
-	new_list = []
-	for i in range(len(imagesRaw)):
-		currentResizedImage = imresize(imagesRaw[i], newSize)
-		new_list.append(currentResizedImage)
-	return new_list
+    new_list = []
+    for i in range(len(imagesRaw)):
+        currentResizedImage = imresize(imagesRaw[i], newSize)
+        new_list.append(currentResizedImage)
+    return new_list
 
 def readImagesNonNormalized(imagesRaw, newSize):
-	new_list = []
-	for i in range(len(imagesRaw)):
-		currentResizedImage = imresizeNonNormalized(imagesRaw[i], newSize)
-		new_list.append(currentResizedImage)
-	return new_list
+    new_list = []
+    for i in range(len(imagesRaw)):
+        currentResizedImage = imresizeNonNormalized(imagesRaw[i], newSize)
+        new_list.append(currentResizedImage)
+    return new_list
 
 def convert(original_list, newSize):
-	# takes the list of images, converts them to 2D matrices, and then converts that to a vector for the classifier
-	new_list = readImages(original_list, newSize)
-	data = numpy.array(new_list)
-	n, nx, ny = data.shape
-	final_data = data.reshape((n, nx * ny))
-	return final_data
+    # takes the list of images, converts them to 2D matrices, and then converts that to a vector for the classifier
+    new_list = readImages(original_list, newSize)
+    data = np.array(new_list)
+    n, nx, ny = data.shape
+    final_data = data.reshape((n, nx * ny))
+    return final_data
+
+def load_data():
+    test_path = '../data/test/'
+    train_path = '../data/train/'
+    
+    train_classes = sorted([dirname for dirname in os.listdir(train_path)], key=lambda s: s.upper())
+    test_classes = sorted([dirname for dirname in os.listdir(test_path)], key=lambda s: s.upper())
+    train_labels = []
+    test_labels = []
+    train_images = []
+    test_images = []
+    for i, label in enumerate(train_classes):
+        for filename in os.listdir(train_path + label + '/'):
+            image = cv2.imread(train_path + label + '/' + filename, 0)
+            train_images.append(image)
+            train_labels.append(i)
+    for i, label in enumerate(test_classes):
+        for filename in os.listdir(test_path + label + '/'):
+            image = cv2.imread(test_path + label + '/' + filename, 0)
+            test_images.append(image)
+            test_labels.append(i)
+            
+    return train_images, test_images, train_labels, test_labels
+
+
+def KNN_classifier(train_features, train_labels, test_features, num_neighbors):
+    # outputs labels for all testing images
+
+    # train_features is an N x d matrix, where d is the dimensionality of the
+    # feature representation and N is the number of training features.
+    # train_labels is an N x 1 array, where each entry is an integer
+    # indicating the ground truth category for each training image.
+    # test_features is an M x d array, where d is the dimensionality of the
+    # feature representation and M is the number of testing features.
+    # num_neighbors is the number of neighbors for the KNN classifier
+
+    # predicted_categories is an M x 1 array, where each entry is an integer
+    # indicating the predicted category for each test image.
+    neigh = KNeighborsClassifier(n_neighbors=num_neighbors)
+    neigh.fit(train_features, train_labels)
+    predicted_categories = neigh.predict(test_features);
+    return predicted_categories
+
+
+def SVM_classifier(train_features, train_labels, test_features, is_linear, svm_lambda):
+    # this function will train a linear svm for every category (i.e. one vs all)
+    # and then use the learned linear classifiers to predict the category of
+    # every test image. every test feature will be evaluated with all 15 svms
+    # and the most confident svm will "win". confidence, or distance from the
+    # margin, is w*x + b where '*' is the inner product or dot product and w and
+    # b are the learned hyperplane parameters.
+
+    # train_features is an N x d matrix, where d is the dimensionality of
+    # the feature representation and N the number of training features.
+    # train_labels is an N x 1 array, where each entry is an integer 
+    # indicating the ground truth category for each training image.
+    # test_features is an M x d matrix, where d is the dimensionality of the
+    # feature representation and M is the number of testing features.
+    # is_linear is a boolean. If true, you will train linear SVMs. Otherwise, you 
+    # will use SVMs with a Radial Basis Function (RBF) Kernel.
+    # svm_lambda is a scalar, the value of the regularizer for the SVMs
+
+    # predicted_categories is an M x 1 array, where each entry is an integer
+    # indicating the predicted category for each test feature.
+    return predicted_categories
+
 
 def imresize(input_image, target_size):
-	# resizes the input image to a new image of size [target_size, target_size]. normalizes the output image
-	# to be zero-mean, and in the [-1, 1] range.
-	dim = (target_size, target_size)
-	img = cv2.resize(input_image, dim)
+    # resizes the input image to a new image of size [target_size, target_size]. normalizes the output image
+    # to be zero-mean, and in the [-1, 1] range.
+    dim = (target_size, target_size)
+    img = cv2.resize(input_image, dim)
 
-	# Normalize image from [-255, 255]
-	output_image = cv2.normalize(img, None, -1, 1, cv2.NORM_MINMAX)
-	return output_image
+    # Normalize image from [-255, 255]
+    output_image = cv2.normalize(img, None, -1, 1, cv2.NORM_MINMAX)
+    return output_image
 
 def imresizeNonNormalized(input_image, target_size):
-	# resizes the input image to a new image of size [target_size, target_size]. normalizes the output image
-	# to be zero-mean, and in the [-1, 1] range.
-	dim = (target_size, target_size)
-	output_image = cv2.resize(input_image, dim)
+    # resizes the input image to a new image of size [target_size, target_size]. normalizes the output image
+    # to be zero-mean, and in the [-1, 1] range.
+    dim = (target_size, target_size)
+    output_image = cv2.resize(input_image, dim)
 
-	return output_image
+    return output_image
 
-def reportAccuracy(true_labels, predicted_labels, label_dict):
-	# generates and returns the accuracy of a model
-	# true_labels is a n x 1 cell array, where each entry is an integer
-	# and n is the size of the testing set.
-	# predicted_labels is a n x 1 cell array, where each entry is an 
-	# integer, and n is the size of the testing set. these labels 
-	# were produced by your system
-	# label_dict is a 15x1 cell array where each entry is a string
-	# containing the name of that category
-	# accuracy is a scalar, defined in the spec (in %)
-	numCorrectPredictions = 0
-	numPredictions = len(predicted_labels) 
 
-	for i in range(numPredictions):
-		if predicted_labels[i] == true_labels[i]:
-			numCorrectPredictions = numCorrectPredictions + 1
+def reportAccuracy(true_labels, predicted_labels):
+    # generates and returns the accuracy of a model
+    # true_labels is a n x 1 cell array, where each entry is an integer
+    # and n is the size of the testing set.
+    # predicted_labels is a n x 1 cell array, where each entry is an 
+    # integer, and n is the size of the testing set. these labels 
+    # were produced by your system
+    # label_dict is a 15x1 cell array where each entry is a string
+    # containing the name of that category
+    # accuracy is a scalar, defined in the spec (in %)
+    numCorrectPredictions = 0
+    numPredictions = len(predicted_labels) 
 
-	accuracy = numCorrectPredictions / numPredictions
-	return accuracy
+    for i in range(numPredictions):
+        if predicted_labels[i] == true_labels[i]:
+            numCorrectPredictions = numCorrectPredictions + 1
+
+    accuracy = numCorrectPredictions / numPredictions
+    return accuracy * 100
 
 def buildDict(train_images, dict_size, feature_type, clustering_type):
-	# this function will sample descrichromeptors from the training images,
-	# cluster them, and then return the cluster centers.
+    # this function will sample descriptors from the training images,
+    # cluster them, and then return the cluster centers.
 
-	# train_images is a n x 1 array of images
-	# dict_size is the size of the vocabulary,
-	# feature_type is a string specifying the type of feature that we are interested in.
-	# Valid values are "sift", "surf" and "orb"
-	# clustering_type is one of "kmeans" or "hierarchical"
+    # train_images is a list of N images, represented as 2D arrays
+    # dict_size is the size of the vocabulary,
+    # feature_type is a string specifying the type of feature that we are interested in.
+    # Valid values are "sift", "surf" and "orb"
+    # clustering_type is one of "kmeans" or "hierarchical"
 
-	# the output 'vocabulary' should be dict_size x d, where d is the 
-	# dimention of the feature. each row is a cluster centroid / visual word.
-	# --- Sampling 
-	sift = cv2.xfeatures2d.SIFT_create()
-	surf = cv2.xfeatures2d.SURF_create()
-	orb = cv2.ORB_create() # K Means Clustering only works when detectAndCompute() is run with raw train_images
+    # the output 'vocabulary' should be a list of length dict_size, with elements of size d, where d is the 
+    # dimention of the feature. each row is a cluster centroid / visual word.
 
-	processed_training_images = readImagesNonNormalized(train_images, 32)
-	all_features = []
-	for i in range(len(processed_training_images)):
-		keypoints_sift, descriptors = sift.detectAndCompute(processed_training_images[i], None)
-		feature = (keypoints_sift, descriptors)
-		if descriptors is None:
-			continue
-		else:
-			for matrix in descriptors:
-				all_features.append(matrix)
-	
-	print(len(all_features))
-	# print(all_features)
+    # NOTE: Should you run out of memory or have performance issues, feel free to limit the 
+    # number of descriptors you store per image.
+    # --- Sampling 
+    sift = cv2.xfeatures2d.SIFT_create()
+    surf = cv2.xfeatures2d.SURF_create()
+    orb = cv2.ORB_create() # K Means Clustering only works when detectAndCompute() is run with raw train_images
+    # orb gets keypoints first, just google orb python
+    # orb then gets descriptors 
 
-	# --- K Means Clustering 
-	# kmeans = cluster.KMeans(dict_size)
-	# kmeans.fit(all_features)
+    processed_training_images = readImagesNonNormalized(train_images, 32)
+    allDescriptors = []
+    for i in range(len(processed_training_images)):
+        keypoints_sift, descriptors = sift.detectAndCompute(processed_training_images[i], None)
+        feature = (keypoints_sift, descriptors)
+        if descriptors is None:
+            continue
+        else:
+            for matrix in descriptors:
+                allDescriptors.append(matrix)
+    
+    print("len(allDescriptors):" + str(len(allDescriptors)))
+    # print(allDescriptors)
 
-	# --- Agglomerative Clustering
-	# npmy = numpy.array(all_features) # orb only, need to feed this into  AgglomerativeClustering().fit
-	# npmy = npmy.reshape(-1, 1) # orb only
-	clustering = AgglomerativeClustering(n_clusters=dict_size).fit(all_features)
-	return clustering
+    # --- K Means Clustering 
+    # clustering = cluster.KMeans(dict_size)
+    # clustering.fit(allDescriptors)
+    # centers = np.array(clustering.cluster_centers_)
+
+    # --- Agglomerative Clustering
+    # npmy = np.array(allDescriptors) # orb only, need to feed this into  AgglomerativeClustering().fit
+    # npmy = npmy.reshape(-1, 1) # orb only
+    # need to find the average feature vector of all features of a certain type, then find the closest actual feature to the average feature
+    clustering = AgglomerativeClustering(n_clusters=dict_size).fit(allDescriptors)
+    centersDict = {}
+    for i in range(len(clustering)):
+        centersDict[clustering[i]] = allDescriptors[i]
+
+    print("clustering.labels_")
+    print(clustering.labels_)
+    print(len(clustering.labels_))
+    print("len(centers)")
+    print(len(centers))
+    return centers
 
 def computeBow(image, vocabulary, feature_type):
-	# extracts features from the image, and returns a BOW representation using a vocabulary
-	# image is 2D array
-	# vocabulary is an array of size dict_size x d
-	# feature type is a string (from "sift", "surf", "orb") specifying the feature
-	# used to create the vocabulary
+    # extracts features from the image, and returns a BOW representation using a vocabulary
 
-	# BOW is the new image representation, a normalized histogram
-	return Bow
+    # image is 2D array
+    # vocabulary is an array of size dict_size x d
+    # feature type is a string (from "sift", "surf", "orb") specifying the feature
+    # used to create the vocabulary
 
-def tinyImages(train_features, test_features, train_labels, test_labels, label_dict):
-	# train_features is a nx1 array of images
-	# test_features is a nx1 array of images
-	# train_labels is a nx1 array of integers, containing the label values
-	# test_labels is a nx1 array of integers, containing the label values
-	# label_dict is a 15x1 array of strings, containing the names of the labels
-	# classResult is a 18x1 array, containing accuracies and runtimes
-	classResult = []
+    # BOW is the new image representation, a normalized histogram
+    return Bow
+    
+def tinyImages(train_features, test_features, train_labels, test_labels):
+    # Resizes training images and flattens them to train a KNN classifier using the training labels
+    # Classifies the resized and flattened testing images using the trained classifier
+    # Returns the accuracy of the system, and the overall runtime (including resizing and classification)
+    # Does so for 8x8, 16x16, and 32x32 images, with 1, 3 and 6 neighbors
 
-	for imageSize in [8, 16, 32]:
-		for numNeighbors in [1, 3, 6]:
-			start_time = time.time()
-			converted_training_set = convert(train_features, imageSize)
-			converted_test_set = convert(test_features, imageSize)
-			predictions = KNN_classifier(converted_training_set, train_labels, converted_test_set, 1)
-			timeTaken = time.time() - start_time
-			accuracy = reportAccuracy(test_labels, predictions, None)
-			classResult.append(accuracy)
-			classResult.append(timeTaken)
+    # train_features is a list of N images, represented as 2D arrays
+    # test_features is a list of M images, represented as 2D arrays
+    # train_labels is a list of N integers, containing the label values for the train set
+    # test_labels is a list of M integers, containing the label values for the test set
 
-	print(classResult)
-	return classResult
-	
+    # classResult is a 18x1 array, containing accuracies and runtimes, in the following order:
+    # accuracies and runtimes for 8x8 scales, 16x16 scales, 32x32 scales
+    # [8x8 scale 1 neighbor accuracy, 8x8 scale 1 neighbor runtime, 8x8 scale 3 neighbor accuracy, 
+    # 8x8 scale 3 neighbor runtime, ...]
+    # Accuracies are a percentage, runtimes are in seconds
+    classResult = []
+
+    for imageSize in [8, 16, 32]:
+        for numNeighbors in [1, 3, 6]:
+            start_time = time.time()
+            converted_training_set = convert(train_features, imageSize)
+            converted_test_set = convert(test_features, imageSize)
+            predictions = KNN_classifier(converted_training_set, train_labels, converted_test_set, numNeighbors)
+            timeTaken = time.time() - start_time
+            accuracy = reportAccuracy(test_labels, predictions)
+            classResult.append(accuracy)
+            classResult.append(timeTaken)
+
+    print(classResult)
+    return classResult
+    
